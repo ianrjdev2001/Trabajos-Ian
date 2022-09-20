9aeda412fbafb913eb55b67ac767e9a1bf468ba4
ianrjdev2001 <109057188+ianrjdev2001@users.noreply.github.com>
2022-09-20 10:04:38 -0600

    Unidad 4 Actividad 5

---
+++ Unidad 4 Actividad/public/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main2.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <div class ="container">
+        <div class="container text-center">      
+        <div class="row justify-content-center">
+            <form action="">
+            <fieldset>
+                <legend>
+                    Dar acceso
+                </legend>
+                
+                    <label>
+                        Email
+                    </label>
+                    <input type="text" name="" placehorder="write here">
+                
+                <label>
+                    Password
+                </label>
+                <input type="password" name="" placehorder="*****">
+                <button type="submit">
+                    Acceder
+                </button>
+            </fieldset>
+            </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
