65533439b8720e59e55aeb9e3d46a3c0a0919954
ianrjdev2001 <109057188+ianrjdev2001@users.noreply.github.com>
2022-10-03 00:37:47 -0600

    Agregar imagen al trabajo

---
+++ Unidad 4 Actividad 18/products/index.php
@@ -0,0 +1,160 @@
+<?php 
+    include '../app/ProductosController.php';
+    $producto = new ProductosController;
+    $productos = $producto->listaProductos();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include "../layouts/head.template.php" ?>
+</head>
+<body>
+    <!-- NAVBAR -->
+    <?php include "../layouts/nav.template.php" ?>
+
+    <!-- CONTAINER -->
+    <div class="container-fluid">
+
+        <div class="row">
+            <!-- SIDEBAR -->
+            <?php include "../layouts/sidebar.template.php" ?>
+
+            <!-- CONTENT -->
+            <div class="col-lg-10 col-sm-12 bg-white">
+
+                <!-- BREAD -->
+                <div class="border-bottom">
+                    <div class="row m-2">
+                        <div class="col">
+                            <h4>Productos</h4>
+                        </div>
+                        <div class="col">
+                            <button class="btn btn-info float-end" data-bs-toggle="modal" data-bs-target="#añadirModal">Añadir producto</button>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- CARD -->
+                <div class="row">
+                <?php foreach($productos as $lista):?> 
+                    <div class="col-md-3 p-2">
+                        <div class="card mb-1" style="width: 18rem;">
+                        <img src="<?php echo $lista->cover ?>" class="card-img-top" alt="...">
+                            <div class="card-body">
+                            <h5 class="card-title text-center"><?php echo $lista->id ?></h5>
+                            <h5 class="card-title text-center"><?php echo $lista->name ?></h5>
+                            <p class="card-text"><?php echo $lista->description ?></p>
+                            <div class="row">
+                                <a class="btn btn-warning col-6" data-bs-toggle="modal" data-bs-target="#editarModal">Editar</a>
+                                <a href="#" class="btn btn-danger col-6" onclick="remove(this)">Eliminar</a>
+                                <a href="detalles.php" class="btn btn-info col-12">Detalles</a>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                    <?php endforeach; ?> 
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal: "AÑADIR" -->
+    <div class="modal fade" id="añadirModal" tabindex="-1" aria-labelledby="añadirModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="añadirModalLabel">Modal title</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+
+            <form method ="post" action="../app/ProductosController.php" enctype ="multipart/form-data">
+                <div class="modal-body">        
+                    
+                        <div class="input-group mb-3">
+                            <input type="file" name="img_producto">
+                        </div>
+
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" id="basic-addon1">@</span>
+                            <input type="text" name="name" class="form-control" placeholder="name" aria-label="Username" aria-describedby="basic-addon1">
+                        </div>
+
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" id="basic-addon1">@</span>
+                            <input type="text" name="slug" class="form-control" placeholder="slug" aria-label="Username" aria-describedby="basic-addon1">
+                        </div>
+
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" id="basic-addon1">@</span>
+                            <input type="text" name="description" class="form-control" placeholder="description" aria-label="Username" aria-describedby="basic-addon1">
+                        </div>
+
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" id="basic-addon1">@</span>
+                            <input type="text" name="features" class="form-control" placeholder="features" aria-label="Username" aria-describedby="basic-addon1">
+                        </div>
+
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" id="basic-addon1">@</span>
+                            <input type="text" name="brand_id" class="form-control" placeholder="brand id" aria-label="Username" aria-describedby="basic-addon1">
+                        </div>
+                </div>
+                <input type="hidden" name="action" value="create">
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Save changes</button>
+                </div>
+            </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal: "EDITAR" -->
+    <div class="modal fade" id="editarModal" tabindex="-1" aria-labelledby="editarModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="editarModalLabel">Modal title</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <?php for ($i=0; $i < 6; $i++) {?> 
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon1">@</span>
+                        <input type="text" class="form-control" placeholder="* * * * *" aria-label="Username" aria-describedby="basic-addon1">
+                    </div>
+                <?php } ?> 
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+            </div>
+        </div>
+    </div>
+
+    <?php include "../layouts/scripts.template.php" ?>
+    <script>
+        function remove (target) {
+            swal({
+            title: "Are you sure?",
+            text: "Once deleted, you will not be able to recover this imaginary file!",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+            })
+            .then((willDelete) => {
+            if (willDelete) {
+                swal("Poof! Your imaginary file has been deleted!", {
+                icon: "success",
+                });
+            } else {
+                swal("Your imaginary file is safe!");
+            }
+            });
+        }
+    </script>
+</body>
+</html>
