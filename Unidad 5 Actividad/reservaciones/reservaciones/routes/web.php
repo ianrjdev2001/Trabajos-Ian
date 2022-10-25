c277609924d692eaee59dfffd5f7805c23e5e236
ianrjdev2001 <109057188+ianrjdev2001@users.noreply.github.com>
2022-10-25 09:58:57 -0600

    Unidad 5 Actividad 12

---
+++ Unidad 5 Actividad/reservaciones/reservaciones/routes/web.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ClientController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+/*Route::get('saludo/{name}', function ($name) {
+    echo "Hola ".$name;
+});*/
+
+
+Route::get('users/',[UserController::class,'index']);
+
+Route::get('users/create',[UserController::class,'create']);
+
+Route::get('users/{id}',[UserController::class,'show']);
+
+Route::post('users/',[UserController::class,'store']);
+
+Route::get('clients/',[ClientController::class,'index']);
+
+Route::get('clients/{id}',[ClientController::class,'show']);
+
+/*Route::get('sum/{num}/{num2}', function ($num,$num2) {
+    echo $num + $num2;
+})->where('num','[0-9]+');*/
+
+/*Route::get('sum/{num}/{num2}', function ($num,$num2) {
+    echo $num + $num2;
+})->where(['num','[0-9]+'],['num2','[0-9]+']);*/
+
+/*Route::get('sum/{num}/{num2}/{num3}', function ($num,$num2,$num3=0) {
+    echo $num + $num2 + $num3;
+})->where(['num' => '[0-9]+','num2' => '[0-9]+']);*/
+
+/*Route::post('sum/', function (Request $request) {
+    echo $num + $num2 + $num3;
+})->where(['num' => '[0-9]+','num2' => '[0-9]+']);*/
