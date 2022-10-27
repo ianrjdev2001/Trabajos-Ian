bbfe4fbb533c87f69940a38f46e1467e0510fbfb
ianrjdev2001 <109057188+ianrjdev2001@users.noreply.github.com>
2022-10-27 10:03:14 -0600

    Unidad 5 Actividad 16

---
+++ Unidad 5 Actividades Jona/reservaciones/reservaciones/database/seeders/ReservationSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Reservation;
+
+class ReservationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $reservation = new  Reservation();
+        $reservation->client_id = '1';
+        $reservation->room_id = '1';
+        $reservation->name = 'Cliente_1';
+        $reservation->save();
+
+        $reservation2 = new  Reservation();
+        $reservation2->client_id = '2';
+        $reservation2->room_id = '2';
+        $reservation2->name = 'Cliente_2';
+        $reservation2->save();
+
+        $reservation3 = new  Reservation();
+        $reservation3->client_id = '2';
+        $reservation3->room_id = '2';
+        $reservation3->name = 'Cliente_2';
+        $reservation3->save();
+    }
+}
